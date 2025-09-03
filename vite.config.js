ca6674153b16d009f3a854bacbfbe3b37769e468
septianwijaya45 <septianwijaya170845@gmail.com>
2025-09-03 20:56:41 +0700

    fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -10,10 +10,4 @@ export default defineConfig({
         }),
         react(),
     ],
-    server: {
-        host: '0.0.0.0', // biar bisa diakses dari luar (bukan cuma localhost)
-        port: 5173,      // default port vite
-        cors: true,      // izinkan cross-origin
-        origin: 'https://tokaku.navadhanarama.com', // samakan dengan domain laravel
-    },
 });
